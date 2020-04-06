cb8f280b61183d3a6dcdbbccee3f82b1be0b79c7
Shannon Ervin <sjervin07@gmail.com>
2020-04-05 23:55:44 -0500

    added index to header component

---
+++ userdirectory/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return(
+<div className="jumbotron text-center p-3 mb-2 bg-secondary text-white">
+  <h1 className="display-4 text-center "><strong>Employee Directory</strong></h1>
+</div>
+    );
+  }
+}
